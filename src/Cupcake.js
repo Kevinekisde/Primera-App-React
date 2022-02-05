e9a8526e47b46879b660a1e1fd32bf4afb996bd2
Kevin Leiva <Kevinomg9@gmail.com>
2022-02-05 12:49:51 -0300

    Actualizacion

--- src/Cupcake.js
+++ src/Cupcake.js
@@ -1,29 +1,44 @@
-
-// Transformacion Componente Clase a Funcion
+import {useState, useEffect} from "react";
 
 const Cupcake = ({color ,sabor ,foto}) => {
 
-    const vender = () => console.log("Vendido")
+    const [vendido, setVendido] = useState(false)
+    const vender = () => {
+        setVendido(true)
+        setReservado(true)
+    }
+
+    const [reservado, setReservado] = useState(false)
+    const reservar = () => setReservado(true)
+
+    useEffect(()=>{
+        console.log("Bienevenido", Date.now())
+    },[vendido])
+
 
     return (
         <div>
             <img src={foto} alt={color}/>
             <p>{`Sabor : ${sabor}`}</p>
             <p>
-                <b>
-                    {
-                        vendido ? "Vendido" : "A la venta"
-                    }
-                </b>
+                <b> Reservado: </b>
+                {
+                    reservado ? "Reservado" : "Libre"
+                }
             </p>
-
+            {
+                !reservado && <button onClick={reservar}>Reservar</button>
+            }
+            <p>
+            <b> Estado: </b>
+            {
+                vendido ? "Vendido" : "A la venta"
+            }
+        </p>
             {
                 !vendido && <button onClick={vender}>Vender</button>
             }
         </div>
     )
-
 }
-
-
 export default Cupcake
