79628595368621314ef6a1d32d4b939f9166ba5a
Kevin Leiva <Kevinomg9@gmail.com>
2022-02-05 12:02:33 -0300

    Actualizacion

--- src/Cupcake.js
+++ src/Cupcake.js
@@ -1,38 +1,28 @@
 
 // Transformacion Componente Clase a Funcion
 
-// Componente de Clase
+const Cupcake = ({color ,sabor ,foto}) => {
 
-import {Component} from "react";
+    const vender = () => console.log("Vendido")
 
-class Cupcake extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            vendido:false,
-        }
-        this.vender=this.vender.bind(this)
-    }
+    return (
+        <div>
+            <img src={foto} alt={color}/>
+            <p>{`Sabor : ${sabor}`}</p>
+            <p>
+                <b>
+                    {
+                        vendido ? "Vendido" : "A la venta"
+                    }
+                </b>
+            </p>
 
-    vender(){
-        this.setState({ 
-            vendido:true
-        })
-    }
+            {
+                !vendido && <button onClick={vender}>Vender</button>
+            }
+        </div>
+    )
 
-
-    render() {
-        console.log(this.state.vendido)
-        return(
-            <div>
-            <img src={this.props.foto} alt={this.props.color}/>
-            <h2>{this.props.color}</h2> 
-            <p>{`Sabor : ${this.props.sabor}`}</p>
-            <p><b>{this.state.vendido ? "Vendido" : "A la venta"}</b></p>
-            {!this.state.vendido && <button onClick={this.vender}>Vender</button>}
-            </div>
-        )
-    }
 }
 
 
